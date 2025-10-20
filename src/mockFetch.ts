6cf2c500968e39303219d1ea0856f639d9911d60
rikeda-cloud <longsichitian648@gmail.com>
2025-10-21 00:56:21 +0900

    [update] testプログラムに作成したdata.tsの内容を反映

--- src/mockFetch.ts
+++ src/mockFetch.ts
@@ -1,6 +1,6 @@
 import { RawResponseData } from "./define";
 import { mockData } from "./data";
 
-export default async function mockFetch(id: number): Promise<RawResponseData[]> {
-  return mockData[id];
+export default async function mockFetch(id: number): Promise<RawResponseData> {
+  return mockData[id].rawData;
 }
