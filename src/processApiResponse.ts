f8009db1fd7be4c8c25ff58209c8019a834d198d
rikeda-cloud <longsichitian648@gmail.com>
2025-10-21 18:53:38 +0900

    [lint]

--- src/processApiResponse.ts
+++ src/processApiResponse.ts
@@ -19,38 +19,44 @@ export default async function processApiResponse(
 
   // INFO StatusCodeが200番以外ならエラーとして処理
   return {
-    status: "error",
-    data: String(res.data)
-  }
+    status: 'error',
+    data: String(res.data),
+  };
 }
 
 async function processOkResponse(rawUsers: RawUser[]): Promise<ResponseData> {
   // INFO 誕生日がnull、または、activeでないユーザーを除外
-  const filteredRawUsers = rawUsers.filter((rawUser: RawUser) => rawUser.birthday !== null && (rawUser.is_active === true || rawUser.is_active === 1));
+  const filteredRawUsers = rawUsers.filter(
+    (rawUser: RawUser) =>
+      rawUser.birthday !== null &&
+      (rawUser.is_active === true || rawUser.is_active === 1),
+  );
 
   const users: User[] = [];
   for (const rawUser of filteredRawUsers) {
     const user_id: number = Number(rawUser.user_id);
-    const friend_ids: number[] = rawUser.friend_ids === null ? [] : rawUser.friend_ids.map((id: string | number) => Number(id));
+    const friend_ids: number[] =
+      rawUser.friend_ids === null
+        ? []
+        : rawUser.friend_ids.map((id: string | number) => Number(id));
     const birthday: Date = convertRawBirthdayToBirthday(rawUser.birthday);
-
 
     const user: User = { user_id, friend_ids, birthday };
     users.push(user);
   }
 
-  return {
-    status: 'success',
-    data: users,
-  };
+  return { status: 'success', data: users };
 }
 
-function convertRawBirthdayToBirthday(rawBirthday: string | number | null): Date {
+function convertRawBirthdayToBirthday(
+  rawBirthday: string | number | null,
+): Date {
   if (typeof rawBirthday === 'number') {
     return new Date(rawBirthday * 1000);
   } else if (typeof rawBirthday === 'string') {
     return new Date(rawBirthday);
   }
 
+  // rawBirthdayが 数値 or 文字列 以外の場合は現在時刻のDateを返す
   return new Date();
 }
