0a402012e616a6b5cb6248ee8afb68b88420e34b
rikeda-cloud <longsichitian648@gmail.com>
2025-10-21 18:47:18 +0900

    [update] テスト9/10通過、Date型の変換のテストケースのみ失敗

--- src/processApiResponse.ts
+++ src/processApiResponse.ts
@@ -1,4 +1,4 @@
-import { ResponseData } from './define';
+import { RawResponseData, ResponseData, RawUser, User } from './define';
 import mockFetch from './mockFetch';
 
 /**
@@ -10,10 +10,47 @@ import mockFetch from './mockFetch';
 export default async function processApiResponse(
   id: number,
 ): Promise<ResponseData> {
-  await mockFetch(id);
+  const res: RawResponseData = await mockFetch(id);
+
+  if (res.status_code === 200) {
+    const rawUsers: RawUser[] = <RawUser[]>res.data;
+    return processOkResponse(rawUsers);
+  }
+
+  // INFO StatusCodeが200番以外ならエラーとして処理
+  return {
+    status: "error",
+    data: String(res.data)
+  }
+}
+
+async function processOkResponse(rawUsers: RawUser[]): Promise<ResponseData> {
+  // INFO 誕生日がnull、または、activeでないユーザーを除外
+  const filteredRawUsers = rawUsers.filter((rawUser: RawUser) => rawUser.birthday !== null && (rawUser.is_active === true || rawUser.is_active === 1));
+
+  const users: User[] = [];
+  for (const rawUser of filteredRawUsers) {
+    const user_id: number = Number(rawUser.user_id);
+    const friend_ids: number[] = rawUser.friend_ids === null ? [] : rawUser.friend_ids.map((id: string | number) => Number(id));
+    const birthday: Date = convertRawBirthdayToBirthday(rawUser.birthday);
+
+
+    const user: User = { user_id, friend_ids, birthday };
+    users.push(user);
+  }
 
   return {
-    status: 'error',
-    data: '処理を完成させ、テストを通るようにしてください',
+    status: 'success',
+    data: users,
   };
+}
+
+function convertRawBirthdayToBirthday(rawBirthday: string | number | null): Date {
+  if (typeof rawBirthday === 'number') {
+    return new Date(rawBirthday * 1000);
+  } else if (typeof rawBirthday === 'string') {
+    return new Date(rawBirthday);
+  }
+
+  return new Date();
 }
