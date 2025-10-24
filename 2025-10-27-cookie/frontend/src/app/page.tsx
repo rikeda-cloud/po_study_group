4accf4b422e27c12036698d88c8d86d337a4b3e2
rikeda-cloud <longsichitian648@gmail.com>
2025-10-25 03:24:59 +0900

    [update] style

--- 2025-10-27-cookie/frontend/src/app/page.tsx
+++ 2025-10-27-cookie/frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Container, Typography, List, ListItem, ListItemButton, ListItemText, Paper } from '@mui/material';
+import { Container, Typography, List, ListItem, ListItemButton, ListItemText, Paper, Box } from '@mui/material';
 import Link from 'next/link';
 
 export default function Home() {
   const steps = [
     {
       href: '/step1',
-      primary: 'Step 1: ClientSessionで完結する買い物カート',
+      primary: 'Step 1: ClientSessionで実現する買い物カート',
       secondary: 'ClientSessionを実現するためのAPIサーバを立て、Cookieを用いた買い物カートを作成しよう'
     },
     {
@@ -21,24 +21,32 @@ export default function Home() {
   ];
 
   return (
-    <Container maxWidth="md" style={{ marginTop: '40px' }}>
-      <Paper elevation={3} style={{ padding: '32px' }}>
-        <Typography variant="h3" component="h1" align="center" gutterBottom>PO勉強会 Cookieについて学ぼう</Typography>
-        <Typography variant="body1" align="center" paragraph>
-          この勉強会はCookieについて学ぶためのものです。Stepを選んでください。
-        </Typography>
-        <List>
-          {steps.map((step) => (
-            <Link href={step.href} key={step.href} passHref style={{ textDecoration: 'none', color: 'inherit' }}>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={step.primary} secondary={step.secondary} />
-                </ListItemButton>
-              </ListItem>
-            </Link>
-          ))}
-        </List>
-      </Paper>
-    </Container>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Container maxWidth="md">
+        <Paper elevation={3} style={{ padding: '32px' }}>
+          <Typography variant="h3" component="h1" align="center" gutterBottom>PO勉強会 Cookieについて学ぼう</Typography>
+          <Typography variant="body1" align="center" paragraph>
+            この勉強会はCookieについて学ぶためのものです。Stepを選んでください。
+          </Typography>
+          <List>
+            {steps.map((step) => (
+              <Link href={step.href} key={step.href} passHref style={{ textDecoration: 'none', color: 'inherit' }}>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary={step.primary} secondary={step.secondary} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Paper>
+      </Container>
+    </Box>
   );
 }
