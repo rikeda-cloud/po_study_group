c36ab9d5ce4106eb72d54d2f6995769f4b78bfa6
tozeki <toshihiro.722105@gmail.com>
2025-10-25 20:29:40 +0900

    fix: ActiveRawUser型の定義を追加し、convertToDateが適切に処理されるよう修正

--- src/processApiResponse.ts
+++ src/processApiResponse.ts
@@ -1,6 +1,14 @@
 import { RawUser, ResponseData, User } from './define';
 import mockFetch from './mockFetch';
 
+// birthdayからnull、is_activeから0|nullをomitした型
+type ActiveRawUser = {
+  user_id: number | string;
+  friend_ids: (number | string)[] | null; // INFO string型の場合、数値に変換可能な文字列
+  birthday: string | number;
+  is_active: true | 1;
+};
+
 /**
  * APIレスポンスを整形する関数。
  *
@@ -24,32 +32,29 @@ export default async function processApiResponse(
   return {
     status: status,
     data: rawUers
-      .filter((e) => e.birthday !== null && e.is_active !== null)
-      .map((e) => convertToUser(e)),
+      .filter((e) => e.birthday && e.is_active)
+      .map((e) => convertToUser(e as ActiveRawUser)),
   };
 }
 
-const convertToUser = (data: RawUser): User => {
+const convertToUser = (data: ActiveRawUser): User => {
   return {
     user_id:
       typeof data.user_id === 'string' ? Number(data.user_id) : data.user_id,
     friend_ids: convertToNumberArray(data.friend_ids),
-    birthday: convertToDate(data.birthday as string | number),
+    birthday: convertToDate(data.birthday),
   };
 };
 
-// BEFORE:
+/**
+ * '/'のままだと国際標準時、'-'だと日本時間で変換されるので、'-'に統一する
+ * number型の場合、引数は秒単位。new Date()はミリ秒単位での引数を要求しているため、*1000してから渡す
+ */
 const convertToDate = (birthday: string | number): Date => {
   return typeof birthday === 'string'
-    ? new Date(Date.parse(birthday))
-    : new Date(birthday);
+    ? new Date(birthday.replace(/\//g, '-'))
+    : new Date(birthday * 1000);
 };
-// AFTER: これでも、1問fail
-// const convertToDate = (birthday: string | number): Date => {
-//   return typeof birthday === 'string'
-//     ? new Date(birthday.replace(/\//g, '-'))
-//     : new Date(birthday * 1000);
-// };
 
 const convertToNumberArray = (
   friend_ids: (number | string)[] | null,
