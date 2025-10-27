d55b8fd9837e7f7ee8ec786fc6647662630a8d9b
rikeda-cloud <longsichitian648@gmail.com>
2025-10-27 17:28:34 +0900

    [WIP] main.py

--- 2025-10-27-cookie/backend/main.py
+++ 2025-10-27-cookie/backend/main.py
@@ -1,18 +1,60 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Cookie
+from fastapi.responses import JSONResponse
+import json
+from pydantic import BaseModel
+
+
+class AddItemId(BaseModel):
+    id: int
+
 
 app = FastAPI()
 
 
 @app.get("/api/step1/items")
-def list_items():
-    return {"Hello": "World"}
+def list_items(cart: Union[str, None] = Cookie(default=None)):
+    content = "[]" if cart is None else cart
+    response = JSONResponse(content=content)
+    return response
 
 
 @app.post("/api/step1/items")
-def add_item():
-    return {"Hello": "World"}
+def add_item(id: AddItemId, cart: Union[str, None] = Cookie(None)):
+    if cart is None:
+        # INFO 初アクセス時
+        response = JSONResponse(content={"cart": "[]"})
+        response.set_cookie(key="cart", value="[]")
+        return response
+
+    cur_cart: list[dict] = json.loads(cart)
+    id_found = False
+    for i, item in enumerate(cur_cart):
+        if item.get("id") == id:
+            cur_cart[i].quantity += 1
+            id_found = True
+    if not id_found:
+        cur_cart.append({"id": id, "quantity": 1})
+
+    cart_data = json.dumps(cur_cart)
+    response = JSONResponse(content={"cart": cart_data})
+    response.set_cookie(key="cart", value=cart_data)
+    return response
 
 
 @app.delete("/api/step1/items/{itemId}")
-def delete_item():
-    return {"Hello": "World"}
+def delete_item(itemId, cart: Union[str, None] = Cookie(default=None)):
+    if cart is None:
+        response = JSONResponse(content="[]")
+        response.set_cookie(key="cart", value="[]")
+        return response
+
+    cur_cart = json.loads(cart)
+    for i, item in enumerate(cur_cart):
+        if item.get("id") == itemId:
+            del cur_cart[i]
+
+    content = json.dumps(cur_cart)
+    response = JSONResponse(content=content)
+    response.set_cookie(key="cart", value=content, max_age=0)
+    return response
