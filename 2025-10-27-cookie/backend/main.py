f7564c95e0187fc4519bc8abe817b1095c2a4f4b
rikeda-cloud <longsichitian648@gmail.com>
2025-10-27 17:31:59 +0900

    [fix] step1の機能全てのAPIを利用可能

--- 2025-10-27-cookie/backend/main.py
+++ 2025-10-27-cookie/backend/main.py
@@ -14,47 +14,46 @@ app = FastAPI()
 
 @app.get("/api/step1/items")
 def list_items(cart: Union[str, None] = Cookie(default=None)):
-    content = "[]" if cart is None else cart
+    content = [] if cart is None else json.loads(cart)
     response = JSONResponse(content=content)
     return response
 
 
 @app.post("/api/step1/items")
-def add_item(id: AddItemId, cart: Union[str, None] = Cookie(None)):
+def add_item(add_item_id: AddItemId, cart: Union[str, None] = Cookie(None)):
     if cart is None:
-        # INFO 初アクセス時
-        response = JSONResponse(content={"cart": "[]"})
-        response.set_cookie(key="cart", value="[]")
-        return response
+        cur_cart = []
+    else:
+        cur_cart: list[dict] = json.loads(cart)
 
-    cur_cart: list[dict] = json.loads(cart)
     id_found = False
     for i, item in enumerate(cur_cart):
-        if item.get("id") == id:
-            cur_cart[i].quantity += 1
+        if item.get("id") == add_item_id.id:
+            cur_cart[i]["quantity"] += 1
             id_found = True
+            break
     if not id_found:
-        cur_cart.append({"id": id, "quantity": 1})
+        cur_cart.append({"id": add_item_id.id, "quantity": 1})
 
     cart_data = json.dumps(cur_cart)
-    response = JSONResponse(content={"cart": cart_data})
+    response = JSONResponse(content=cur_cart)
     response.set_cookie(key="cart", value=cart_data)
     return response
 
 
 @app.delete("/api/step1/items/{itemId}")
-def delete_item(itemId, cart: Union[str, None] = Cookie(default=None)):
+def delete_item(itemId: int, cart: Union[str, None] = Cookie(default=None)):
     if cart is None:
-        response = JSONResponse(content="[]")
-        response.set_cookie(key="cart", value="[]")
-        return response
+        cur_cart = []
+    else:
+        cur_cart = json.loads(cart)
 
-    cur_cart = json.loads(cart)
     for i, item in enumerate(cur_cart):
         if item.get("id") == itemId:
             del cur_cart[i]
+            break
 
     content = json.dumps(cur_cart)
-    response = JSONResponse(content=content)
-    response.set_cookie(key="cart", value=content, max_age=0)
+    response = JSONResponse(content=cur_cart)
+    response.set_cookie(key="cart", value=content)
     return response
